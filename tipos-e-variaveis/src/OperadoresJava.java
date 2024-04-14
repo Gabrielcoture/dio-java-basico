3b13bc3b8a30b70aee1cb6b21f008918ab06fd53
Gabrielcoture <gabrielcoture@gmail.com>
2024-04-14 15:43:46 -0300

    Projetinho smart TV - Java Básico

---
+++ tipos-e-variaveis/src/OperadoresJava.java
@@ -0,0 +1,13 @@
+import javax.print.event.PrintEvent;
+
+/**
+ * OperadoresJava
+ */
+public class OperadoresJava {
+
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        
+        System.out.println(nomeCompleto);
+    }
+}
