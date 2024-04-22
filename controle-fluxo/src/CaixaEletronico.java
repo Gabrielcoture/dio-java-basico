2013f1dad440ad6403bdb1ba2adc1bcae93a34b0
Gabrielcoture <gabrielcoture@gmail.com>
2024-04-22 06:53:04 -0300

    condicional encadeada

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -2,12 +2,15 @@ public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 50.0;
 
         if (valorSolicitado < saldo) {
+            
             saldo = saldo - valorSolicitado;
-        }
+            System.out.println("Novo saldo: " + saldo);
+
+        }else
     
-        System.out.println("Hello, World!");
+        System.out.println("Saldo insuficiente");
     }
 }
