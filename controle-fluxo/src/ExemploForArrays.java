3884fa067f4dfd664b299f6958e6c23fea097279
Gabrielcoture <gabrielcoture@gmail.com>
2024-04-23 07:01:17 -0300

    for em arrays

---
+++ controle-fluxo/src/ExemploForArrays.java
@@ -0,0 +1,22 @@
+public class ExemploForArrays {
+        public static void main(String[] args) {
+            String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+        
+            for (int x=0; x<alunos.length; x++) {
+                System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+            }
+        
+        }
+
+        
+        public static void main2(String[] args) {
+                String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+            
+                for (int x=0; x<alunos.length; x++) {
+                    System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+                }
+            
+        }
+            
+}
+
