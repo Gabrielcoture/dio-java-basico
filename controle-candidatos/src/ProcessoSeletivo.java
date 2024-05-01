d635e1355e91f0bc8f2d4d8ff9f40b9c42f719ae
Gabrielcoture <gabrielcoture@gmail.com>
2024-05-01 10:44:23 -0300

    Aqui eu tentei usar Maven e não deu boa

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,18 +1,30 @@
+import com.github.javafaker.Faker;
 
 
 public class ProcessoSeletivo {
-    public static void main(String[] args) throws Exception {
-        analisarCandidato(2500);;
+    public static void main(String[] args) throws Exception {    
+        Faker faker = new Faker(); // Criar uma instância do Faker
+        
+        int numCandidatos = ThreadLocalRandom.current().nextInt(5, 20); // Gerar um número aleatório de candidatos
+    
+        System.out.println("Número total de candidatos: " + numCandidatos);
+        for (int i = 0; i < numCandidatos; i++) {
+            String nome = faker.name().fullName(); // Gerar um nome aleatório
+            double salarioPretendido = ThreadLocalRandom.current().nextDouble(1500.0, 3000.0); // Gerar um salário aleatório entre 1500.0 e 3000.0
+            System.out.println("Nome: " + nome + ", Salário Pretendido: " + salarioPretendido);
+            analisarCandidato(salarioPretendido);
+            System.out.println("--------------------------------------------");
+        }
     }
 
-    static void analisarCandidato(double SalarioPretendido) {
+    static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
-        if(salarioBase > SalarioPretendido) {
+        if (salarioBase > salarioPretendido) {
             System.out.println("Ligar para o candidato");
-            }else if (salarioBase==SalarioPretendido) {
-                System.out.println("Ligar para o candidato com contra proposta");
-                }else {
-                    System.out.println("Aguardando resultado dos demais candidatos");
-                }
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando resultado dos demais candidatos");
+        }
     }
 }
